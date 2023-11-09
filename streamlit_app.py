76c044a56f9c7b6c5a079c8f87727d3b083b159b
Archit Kaila <archit.kaila@gmail.com>
2023-11-09 14:17:12 -0500

    added file upload function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from pdf_populate import populate_pdf, read_csv, read_excel, zip_folder
+import os
+import glob
 
 ## Landing page UI
 def run_UI():
@@ -14,46 +16,58 @@ def run_UI():
     # Set the page title
     st.header("Fuqua Course Enrollment Forms")
 
-    # Display the data
-    df = pd.read_excel('./data/Fuqua Form Automation Excel.xlsx')
-    df = df.drop_duplicates(subset='Student ID#', keep='last')
-    df.rename(columns={'Student ID#': 'Student ID', 'Full name':'Student Name', 'Duke e-mail address':"Email ID", "Approve/Reject":"Status", "Credit/Audit":"Enrollment Type"}, inplace=True)
-    st.table(data=df[["Student Name", "Email ID", "Enrollment Type", "Status"]])
-
     # Sidebar menu
     with st.sidebar:
-        st.subheader("Student Authorization Forms")
+        st.subheader("Student Authorization Forms Menu")
+
+        # Data file uploader
+        data_file = st.file_uploader("Upload the Student Info Excel File", type="xlsx", key="upload", accept_multiple_files=False)
 
         # Process the document
         if st.button("Generate Forms ✨"):
-            # Add a progress spinner
-            with st.spinner("Processing"):
-                
-                # Read the data
-                data = read_excel('./data/Fuqua Form Automation Excel.xlsx')
-
-                # Loop over the data
-                for i, row in enumerate(data):
-                    input_pdf_path = './data/template_fuqua.pdf'
-                    output_pdf_path = f'results/{row["Full name"]}.pdf'
+            # Check if the data file is uploaded
+            if data_file is not None:
+                # Add a progress spinner
+                with st.spinner("Processing"):
                     
-                    # Populate the PDF with the data
-                    populate_pdf(input_pdf_path, output_pdf_path, row)
-                    print(f"[INFO] Generated PDF: {row['Full name']}")
-                
-                folder_path = "./results"  # Replace with your folder path
+                    # Read the data
+                    data = read_excel(data_file)
 
-                ## Zip the pdfs
-                zip_folder(folder_path, "Student_Forms")
-                
-                ## Download the zip file
-                with open("Student_Forms.zip", "rb") as fp:
-                    btn = st.download_button(
-                        label="Download Forms 📥",
-                        data=fp,
-                        file_name="Student_Forms.zip",
-                        mime="application/zip"
-                    )
+
+                    # Delete the files in the results folder
+                    files = glob.glob('./results/*')
+                    for f in files:
+                        os.remove(f)
+
+                    # Loop over the data
+                    for i, row in enumerate(data):
+                        input_pdf_path = './data/full_template_fuqua.pdf'
+                        output_pdf_path = f'results/{row["Full name"]}.pdf'
+                        
+                        # Populate the PDF with the data
+                        populate_pdf(input_pdf_path, output_pdf_path, row)
+                        print(f"[INFO] Generated PDF: {row['Full name']}")
+                    
+                    folder_path = "./results"  # Replace with your folder path
+
+                    ## Zip the pdfs
+                    zip_folder(folder_path, "Student_Forms")
+                    
+                    ## Download the zip file
+                    with open("Student_Forms.zip", "rb") as fp:
+                        btn = st.download_button(
+                            label="Download Forms 📥",
+                            data=fp,
+                            file_name="Student_Forms.zip",
+                            mime="application/zip"
+                        )
+        
+    # Display the data
+    if data_file is not None:
+        df = pd.read_excel(data_file)
+        df = df.drop_duplicates(subset='Student ID#', keep='last')
+        df.rename(columns={'Student ID#': 'Student ID', 'Full name':'Student Name', 'Duke e-mail address':"Email ID", "Approve/Reject":"Status", "Credit/Audit":"Enrollment Type"}, inplace=True)
+        st.table(data=df[["Student Name", "Email ID", "Enrollment Type", "Status"]])
 
 if __name__ == "__main__":
     run_UI()
